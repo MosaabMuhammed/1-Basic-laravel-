e1c74c1b1c5dca7255e12f4b0ddd1071168e0618
Mosaab Muhammed <mosaabmuhammed@outlook.com>
2018-06-20 00:29:56 +0200

    adding the form in contact view

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,7 +1,23 @@
 @extends('layouts.master')
 
 @section('content')
-    <div class="container">
+    <div class="col-md-8 col-lg-8">
         <h1>Contact</h1>
+
+        {!! Form::open(['url' => 'contact/submit', 'method' => 'POST']) !!}
+            <div class="form-group">
+                {{ Form::label('name', 'Name:') }}
+                {{ Form::text('name', '', ['class' => 'form-control', 'placeholder' => 'Your Name...']) }}
+            </div>
+            <div class="form-group">
+                {{ Form::label('email', 'Email:') }}
+                {{ Form::text('email', '', ['class' => 'form-control', 'placeholder' => 'Your E-mail...']) }}
+            </div>
+            <div class="form-group">
+                {{ Form::label('message', 'Message:') }}
+                {{ Form::textarea('message', '', ['class' => 'form-control', 'placeholder' => 'Your Message...']) }}
+            </div>
+                {{ Form::submit('Submit', ['class' => 'btn btn-primary']) }}
+        {!! Form::close() !!}
     </div>
 @endsection
